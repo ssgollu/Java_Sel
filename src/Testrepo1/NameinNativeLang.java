f6073f490c69abad17ff090911693829f58d1795
Shar <ssgollu@outlook.com>
2024-02-14 16:59:54 -0600

    Code for name in Telugu

---
+++ src/Testrepo1/NameinNativeLang.java
@@ -0,0 +1,29 @@
+package Testrepo1;
+
+public class NameinNativeLang {
+
+	public static void main(String[] args) {
+		char char1 = '\u0C37'; //ష
+		char char2 = '\u0C30'; //ర	
+		char char3 = '\u0C4D'; //్	
+		char char4 = '\u0C2E'; //మ
+		char char5 = '\u0C3F'; //ి
+		char char6 = '\u0C32'; //ల
+		char char7 = '\u0C38'; //స
+		char char8 = '\u0C3E'; //ా	
+		char char9 = '\u0C2F'; //య
+		char char10 = '\u0C3F'; //ి
+		char char11 = '\u0C17'; //గ	
+		char char12 = '\u0C4A'; //ొ
+		char char13 = '\u0C32'; // ల
+		char char14 = '\u0C4D'; // ్
+		char char15 = '\u0C32'; // ల
+		char char16 = '\u0C41'; // ు
+
+		System.out.println(char1 + "" + char2 + "" + char3 + "" + char4 + "" + char5 + "" + char6 + " " + char7 + ""
+				+ char8 + "" + char9 + "" + char10 + " " + char11 + "" + char12 + "" + char13 + "" + char14 + ""
+				+ char15 + "" + char16);
+
+	}
+
+}
