a8e0c207200dfc21f2a4d6c40972fa55242500c4
Shar <ssgollu@outlook.com>
2024-02-19 21:00:23 -0600

    Convert Celsius to Farenheit and Kelvin

---
+++ src/Testrepo1/CelsiusTemperatureConversion.java
@@ -0,0 +1,19 @@
+package Testrepo1;
+import java.util.Scanner;
+
+
+public class CelsiusTemperatureConversion {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Enter temperature in Celsius : ");
+		Scanner obj = new Scanner(System.in);
+		double Celsius = obj.nextDouble();
+		System.out.println(Celsius + " Celsius = " + ((1.8*Celsius)+32) + " Farenheit");
+		System.out.println(Celsius + " Celsius = " + (Celsius+273) + " Kelvin");
+
+		
+		};
+		
+	}
+
