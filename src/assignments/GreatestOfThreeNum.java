d49bc3b7d6f843e2fb0ebd1de172c081d785d87c
Shar <ssgollu@outlook.com>
2024-02-25 20:05:16 -0600

    Greatest Of Three Numbers

---
+++ src/assignments/GreatestOfThreeNum.java
@@ -0,0 +1,27 @@
+package assignments;
+
+import java.util.Scanner;
+
+public class GreatestOfThreeNum {
+
+	public static void main(String[] args) {
+		Scanner obj = new Scanner(System.in);
+		System.out.println("Enter first number :");
+		int num1 = obj.nextInt();
+		System.out.println("Enter second number :");
+		int num2 = obj.nextInt();
+		System.out.println("Enter third number :");
+		int num3 = obj.nextInt();
+		
+		if (num1 > num2 && num1 > num3) {
+			System.out.println(num1 + " is greatest of all 3");
+		} else if (num2 > num3 && num2 > num1) {
+			System.out.println(num2 + " is greatest of all 3");
+		}else {
+			System.out.println(num3 + " is greatest of all 3");
+		} 
+	
+	
+	}
+
+}
