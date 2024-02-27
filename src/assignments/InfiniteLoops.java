627781359688b45196c2251d4124c6c5dea5cd73
Shar <ssgollu@outlook.com>
2024-02-27 10:29:12 -0600

    3 infinite loops

--- src/assignments/InfiniteLoops.java
+++ src/assignments/InfiniteLoops.java
@@ -1,10 +1,39 @@
 package assignments;
 
+import java.util.Scanner;
+
+import net.bytebuddy.agent.builder.AgentBuilder.CircularityLock.Default;
+
 public class InfiniteLoops {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Enter a number between 1,2,3");
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		switch(num) {
+		case 1:
+		
+			System.out.println("Invoking Infinite loop 1 :");
+			for(int i =0 ;true;i++)
+				System.out.println("Loop 1");
+		case 2:
+		
+			System.out.println("Invoking Infinite loop 2 :");
+			while(true)
+				System.out.println("Loop 2");
+		case 3:
+		
+			System.out.println("Invoking Infinite loop 3 :");
+			do {
+				System.out.println("Loop 3");
+			}
+			while(true);	
+		default :
+			System.out.println("Enter only the valid numbers.");
+	}
+		
+	
+	
 	}
 
 }
