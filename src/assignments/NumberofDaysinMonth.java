72ca93b26a53b49b1a6ebb317a33282d056fc282
Shar <ssgollu@outlook.com>
2024-02-27 09:33:05 -0600

    Added leap year logic

--- src/assignments/NumberofDaysinMonth.java
+++ src/assignments/NumberofDaysinMonth.java
@@ -9,20 +9,28 @@ public class NumberofDaysinMonth {
 		Scanner obj = new Scanner(System.in);
 		int num_entered = obj.nextInt();
 		int num_of_days = 0;
+		System.out.println("Enter the year :");
+		int year = obj.nextInt();
 		switch (num_entered) {
 		case 1:
 		case 3:
 		case 5:
 		case 7:
-		case 8:
+		case 8: 
 		case 10:
 		case 12:
 		num_of_days = 31;	
 		System.out.println("Num of days are " + num_of_days);
 		break;
 		case 2:
-			num_of_days = 28;
+			if((year%4==0 && year%100!=0) || year%400 ==0) {
+			num_of_days = 29;
 			System.out.println("Num of days are " + num_of_days);
+			}
+			else {
+				num_of_days = 28;
+				System.out.println("Num of days are " + num_of_days);
+			}
 		break;
 		default:
 			num_of_days = 30;
